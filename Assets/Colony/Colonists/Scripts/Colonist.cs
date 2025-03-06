f95079fcc4ca2dd372a58dfbbcce6fe51cb9417b
tzcrowdis <100492586+tzcrowdis@users.noreply.github.com>
2025-03-05 17:36:44 -0700

    Organization + AI package install

---
+++ Assets/Colony/Colonists/Scripts/Colonist.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colonist : MonoBehaviour
+{
+    // limbs
+    GameObject head;
+    GameObject armR;
+    GameObject armL;
+    GameObject legR;
+    GameObject legL;
+
+    // occupation
+
+    // mental status
+    enum MentalStatus
+    {
+        Hungry,
+        Bored,
+        Sleepy
+    }
+}
